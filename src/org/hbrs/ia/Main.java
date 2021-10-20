3e188212f038f81eb9039580f124c0b4d5134fed
mhoeni2s <mario.hoenighausen@smail.inf.h-brs.de>
2021-10-20 12:23:42 +0200

    Changed POM file Java and MongoDB Driver Version

--- src/org/hbrs/ia/Main.java
+++ src/org/hbrs/ia/Main.java
@@ -9,14 +9,14 @@ import org.bson.Document;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
-        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        /*MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
         MongoDatabase database = mongoClient.getDatabase("food");
         MongoCollection<Document> fruits = database.getCollection("fruits");
         for (Document document : fruits.find()) {
             System.out.println("Name of the fruit " +document.get("name"));
             System.out.println("Origin of the fruit " +document.get("origin"));
             System.out.println("Price of the fruit " +document.get("price"));
-        }
+        }*/
 
 
     }
