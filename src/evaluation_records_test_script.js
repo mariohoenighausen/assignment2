aaedf7b3ca6a670b413c81572c6d9c317d9b5d2f
mhoeni2s <mario.hoenighausen@smail.inf.h-brs.de>
2021-10-21 15:59:50 +0200

    Added minimal round-tripping test in evaluation_records_test_script.js

--- src/evaluation_records_test_script.js
+++ src/evaluation_records_test_script.js
@@ -1,4 +1,5 @@
 db = connect('mongodb://localhost:27017/evaluation_records_test');
+
 db.createCollection("salesman");
 db.salesman.insertOne({sid:1,firstName:"Peter",lastName:"Griffin",dob:"20.08.1995",experience:"CEO Erfahrungen"});
 db.createCollection("evaluation_record");
@@ -6,5 +7,12 @@ db.evaluation_record.insertOne({erid:1,actual_value:850,year:2021,sid:1,goalID:1
 
 db.createCollection("goal");
 db.goal.insertOne({goalID:1,goal_description:"Sell useless stuff",target_Value:1000});
-db.performance_records.find({"sid":{$lt: 2} });
-db.drop("salesmen");
+
+db.salesman.find()
+db.evaluation_record.find()
+db.goal.find()
+
+db.evaluation_record.find({"sid":{$lt: 2} });
+//db.drop("salesman");
+//db.drop("evaluation_record");
+//db.drop("goal")
