dc93a06243d2ded6be6baa9dda405b225c2f3b43
alextewes <alexandertewes7@gmail.com>
2021-10-25 15:58:27 +0200

    disable logging in commandline

--- src/org/hbrs/ia/Main.java
+++ src/org/hbrs/ia/Main.java
@@ -13,11 +13,16 @@ import org.hbrs.ia.model.SalesMan;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Updates.*;
 
 public class Main {
     public static void main(String[] args) {
+        Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+        mongoLogger.setLevel(Level.SEVERE);
         EingabeDialog eingabeDialog = new EingabeDialog();
         eingabeDialog.startEingabe();
     }
