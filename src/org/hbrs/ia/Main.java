173033a085982d97ca6bacd8de2ec34e954f2856
mariohoenighausen <mario.hoenighausen@smail.inf.h-brs.de>
2021-10-25 16:05:26 +0200

    Removed unnecessary import statements

--- src/org/hbrs/ia/Main.java
+++ src/org/hbrs/ia/Main.java
@@ -1,23 +1,8 @@
 package org.hbrs.ia;
-
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.UpdateOptions;
-import org.bson.Document;
 import org.hbrs.ia.control.EingabeDialog;
-import org.hbrs.ia.control.ManagePersonalController;
-import org.hbrs.ia.model.EvaluationRecord;
-import org.hbrs.ia.model.SalesMan;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import static com.mongodb.client.model.Filters.*;
-import static com.mongodb.client.model.Updates.*;
 
 public class Main {
     public static void main(String[] args) {
